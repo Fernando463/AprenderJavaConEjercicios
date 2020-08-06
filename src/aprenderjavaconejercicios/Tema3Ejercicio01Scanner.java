0d0f44089f3a418d279927df9997f5a90e917e22
Fernando Gonzalez <facturas5454@gmail.com>
2020-08-06 22:24:28 +0200

    04/08/2020.- Otro commit.

---
+++ src/aprenderjavaconejercicios/Tema3Ejercicio01Scanner.java
@@ -0,0 +1,30 @@
+/*
+ * Programa que nos pide dos números, los introducimos por teclado
+ * y nos devuelve su producto, empleando la clase Scanner.
+ */
+package aprenderjavaconejercicios;
+
+import java.util.Scanner;
+
+
+public class Tema3Ejercicio01Scanner {
+  
+  public static void main(String[] args){
+    
+    Scanner s = new Scanner(System.in);
+    
+    System.out.print("Introduce dos números separados por un espacio: ");
+    
+    int numero1= s.nextInt();
+    int numero2= s.nextInt();
+    
+    int producto;
+    
+    producto= numero1 * numero2;
+    
+    System.out.println("El producto de los dos números es: " + producto);
+    
+    
+  }
+  
+}
