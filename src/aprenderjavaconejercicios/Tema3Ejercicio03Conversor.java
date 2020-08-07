91ffa1b19181b54062d798510620851a761bc073
Fernando Gonzalez <facturas5454@gmail.com>
2020-08-07 20:03:06 +0200

    07/08/2020.- Más ejercicios.

---
+++ src/aprenderjavaconejercicios/Tema3Ejercicio03Conversor.java
@@ -0,0 +1,24 @@
+/*
+ * Conversor de peseta a euros. La cantidad a convertir
+ * debe ser introducida por teclado.
+ */
+package aprenderjavaconejercicios;
+
+import java.util.Scanner;
+
+public class Tema3Ejercicio03Conversor {
+  
+  public static void main(String[] args){
+    
+    Scanner s = new Scanner(System.in);
+    
+    System.out.print("Introduce la cantidad de pesetas a convertir en euros y pulsa INTRO:");
+    
+    double numero=s.nextDouble();
+    
+    double total = (numero/166.386);
+    
+    System.out.println(numero+" pesetas son"+total+" euros");
+  }
+  
+}
